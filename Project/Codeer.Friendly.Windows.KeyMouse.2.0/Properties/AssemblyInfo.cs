c228d91ceb658275997e5a0c98e63a72704580a2
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2025-09-13 16:56:44 +0900

    version up

--- Project/Codeer.Friendly.Windows.KeyMouse.2.0/Properties/AssemblyInfo.cs
+++ Project/Codeer.Friendly.Windows.KeyMouse.2.0/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Codeer.Friendly.Windows.KeyMouse.2.0")]
@@ -15,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ca989875-6e96-49ac-9b39-b5ac25ed6efe")]
 
-[assembly: AssemblyVersion("1.3.19.0")]
-[assembly: AssemblyFileVersion("1.3.19.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
--- Project/Codeer.Friendly.Windows.KeyMouse.3.5/Properties/AssemblyInfo.cs
+++ Project/Codeer.Friendly.Windows.KeyMouse.3.5/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Codeer.Friendly.Windows.KeyMouse.3.5")]
@@ -15,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("cd0f3543-6ff1-4713-b5a9-be126f20f56d")]
 
-[assembly: AssemblyVersion("1.3.19.0")]
-[assembly: AssemblyFileVersion("1.3.19.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
